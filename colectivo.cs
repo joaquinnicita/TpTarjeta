31438895da59efa516cd726bcdc15b65ac04b887
Joaquin <joaquinnicita@gmail.com>
2024-09-29 16:08:05 -0300

    iteracion 2 arreglo

--- colectivo.cs
+++ colectivo.cs
@@ -1,70 +1,23 @@
-using System;
-using BoletoNamespace;
-using ColectivoNamespace;
-using TarjetaNamespace;
+  using System;
+  using TarjetaNamespace;
+  using BoletoNamespace;
 
-class Program
-{
-
-    public static void Main(string[] args)
-    {
-        boleto boleto = new boleto();
-        colectivo colectivo = new colectivo();
-        tarjeta tarjeta = null;
-
-        while (true)
-        {
-            Console.WriteLine("Tipo de tarjeta a usar: ");
-            Console.WriteLine("1 Normal");
-            Console.WriteLine("2 Medio Boleto");
-            Console.WriteLine("3 Franquicia Completa");
-            string tipo_tarjeta = Console.ReadLine();
-
-            switch (tipo_tarjeta)
-            {
-                case "1":
-                    tarjeta = new tarjeta();
-                    break;
-                case "2":
-                    tarjeta = new MedioBoleto();
-                    break;
-                case "3":
-                    tarjeta = new FranquiciaCompleta();
-                    break;
-
-                default:
-                    Console.WriteLine("Opcion no valida");
-                    break;
-            }
-            break;
-
-        }
-
-
-        while (true)
-        {
-            Console.WriteLine("Ingrese una opcion");
-            Console.WriteLine("1: Cargar saldo");
-            Console.WriteLine("2: Pagar boleto");
-            Console.WriteLine("saldo actual: " + tarjeta.saldo);
-            
-            string opcion = Console.ReadLine();
-            {
-                switch (opcion)
-                {
-                    case "1":
-                        Console.WriteLine("Ingrese el monto a cargar");
-                        int monto = int.Parse(Console.ReadLine());
-                        tarjeta.cargarSaldo(monto);
-                        break;
-                    case "2":
-                        colectivo.PagarCon(tarjeta);
-                        break;
-                    default:
-                        Console.WriteLine("Opcion no valida");
-                        break;
-                }
-            }
-        }
-    }
-}
+  namespace ColectivoNamespace
+  {
+      public class colectivo {
+          boleto boleto = new boleto();
+          public virtual void PagarCon (tarjeta tarjeta)
+          {
+              if (tarjeta.saldo >= tarjeta.precioBoleto(boleto.precio) - 480)
+              {
+                  tarjeta.saldo -= tarjeta.precioBoleto(boleto.precio);
+                  Console.WriteLine("Pago realizado con éxito.");
+              }
+              else
+              {
+                  Console.WriteLine("Saldo insuficiente!");
+              }
+          }              
+          
+      }
+  }
--- main.cs
+++ main.cs
@@ -3,31 +3,68 @@ using BoletoNamespace;
 using ColectivoNamespace;
 using TarjetaNamespace;
 
-class Program {
-  
-  public static void Main (string[] args) {
-    tarjeta tarjeta = new tarjeta();
-    boleto boleto = new boleto();
-    colectivo colectivo = new colectivo();
-      
-    Console.WriteLine ("Ingrese una opcion");
-    Console.WriteLine ("1: Cargar saldo");
-    Console.WriteLine ("2: Pagar boleto");
-    
-    string opcion = Console.ReadLine();
-    while(true){
-    switch (opcion) {
-    case "1" :
-        Console.WriteLine("Ingrese el monto a cargar");
-        int monto = int.Parse(Console.ReadLine());
-        tarjeta.cargarSaldo(monto);
-        break;
-    case "2":
-        colectivo.PagarCon(tarjeta);
-        break;
-    default:
-        Console.WriteLine("Opcion no valida");
-        break;
-        }}
-  }
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        boleto boleto = new boleto();
+        colectivo colectivo = new colectivo();
+        tarjeta tarjeta = null;
+
+        while (true)
+        {
+            Console.WriteLine("Tipo de tarjeta a usar: ");
+            Console.WriteLine("1 Normal");
+            Console.WriteLine("2 Medio Boleto");
+            Console.WriteLine("3 Franquicia Completa");
+            string tipo_tarjeta = Console.ReadLine();
+
+            switch (tipo_tarjeta)
+            {
+                case "1":
+                    tarjeta = new tarjeta();
+                    break;
+                case "2":
+                    tarjeta = new MedioBoleto();
+                    break;
+                case "3":
+                    tarjeta = new FranquiciaCompleta();
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
+            break;
+
+        }
+
+
+        while (true)
+        {
+            Console.WriteLine("Ingrese una opcion");
+            Console.WriteLine("1: Cargar saldo");
+            Console.WriteLine("2: Pagar boleto");
+            Console.WriteLine("saldo actual: " + tarjeta.saldo);
+            
+            string opcion = Console.ReadLine();
+            {
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese el monto a cargar");
+                        int monto = int.Parse(Console.ReadLine());
+                        tarjeta.cargarSaldo(monto);
+                        break;
+                    case "2":
+                        colectivo.PagarCon(tarjeta);
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+                }
+            }
+        }
+    }
 }
