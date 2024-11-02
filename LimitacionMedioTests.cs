50b35c029d1bd88c3d4f60b42a9f7a355c8148ad
TobiasMan05 <tobiasman7@gmail.com>
2024-11-02 17:47:31 -0300

    LimitacionMedioTests.cs - Iteracion3

---
+++ LimitacionMedioTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using System;
+using System.Threading;
+using TarjetaNamespace;
+using ColectivoNamespace;
+
+namespace Tests
+{
+    public class LimitacionMedioBoleto
+    {
+        [Test]
+        public void Test_5Minutos()
+        {
+            var tarjeta = new MedioBoleto();
+            tarjeta.cargarSaldo(2000); 
+
+            bool primerViaje = tarjeta.TarjetaUsos(tarjeta);
+            bool segundoViaje = tarjeta.TarjetaUsos(tarjeta);
+
+            Assert.IsTrue(primerViaje, "El primer viaje debería estar permitido.");
+            Assert.IsFalse(segundoViaje, "No debería permitir otro viaje antes de los 5 minutos.");
+        }
+        [Test]
+        public void Test_ViajePermitidoDespuesDe5Minutos()
+        {
+            var tarjeta = new MedioBoleto();
+            tarjeta.cargarSaldo(2000);
+
+            bool primerViaje = tarjeta.TarjetaUsos(tarjeta);
+            tarjeta.ultimoUso = DateTime.Now.AddMinutes(-5);
+            bool segundoViaje = tarjeta.TarjetaUsos(tarjeta);
+
+            Assert.IsTrue(segundoViaje, "Debería permitir realizar otro viaje después de 5 minutos.");
+        }
+    }
+}
