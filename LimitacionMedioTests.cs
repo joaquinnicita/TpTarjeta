969fe54e3f8c7457c1689c727ca3404643b69605
TobiasMan05 <tobiasman7@gmail.com>
2024-11-02 18:18:01 -0300

    LimitacionMedioTests.cs - Iteracion3

--- LimitacionMedioTests.cs
+++ LimitacionMedioTests.cs
@@ -30,7 +30,26 @@ namespace Tests
             tarjeta.ultimoUso = DateTime.Now.AddMinutes(-5);
             bool segundoViaje = tarjeta.TarjetaUsos(tarjeta);
 
-            Assert.IsTrue(segundoViaje, "Debería permitir realizar otro viaje después de 5 minutos.");
+            Assert.IsTrue(segundoViaje, "Debería permitir realizar otro viaje despues de 5 minutos.");
         }
+
+        [Test]
+        public void Viajes4porDiaTest()
+        {
+            var tarjeta = new MedioBoleto();
+            var colectivo = new Colectivo();
+            tarjeta.cargarSaldo(9000);
+
+            for (int i = 0; i < 4; i++)
+            {
+                colectivo.PagarCon(tarjeta);
+                tarjeta.ultimoUso = DateTime.Now.AddMinutes(-5);
+            }
+
+            colectivo.PagarCon(tarjeta);  
+
+            Assert.AreEqual(9000 - (470*4) - 940, tarjeta.saldo, "El test no deberia funcionar ya que la consigna no pide esta limitacion.");
+        }
+
     }
 }
