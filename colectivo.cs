2d981134f45f26585e5d516c031936336c6ef8fd
joaquin nicita <82076819+joaquinnicita@users.noreply.github.com>
2024-11-03 16:39:54 -0300

    calcular bien el precio

--- colectivo.cs
+++ colectivo.cs
@@ -1,24 +1,38 @@
-  using System;
-  using TarjetaNamespace;
-  using BoletoNamespace;
+using System;
+using TarjetaNamespace;
+using BoletoNamespace;
 
-  namespace ColectivoNamespace
-  {
-      public class colectivo {
-          public string linea = "102 144";
-          boleto boleto = new boleto();
-          public virtual void PagarCon (tarjeta tarjeta, int precio)
-          {
-              if (tarjeta.saldo >= precio - 480)
-              {
-                  tarjeta.saldo -= precio;
-                  Console.WriteLine("Pago realizado con éxito.");
-              }
-              else
-              {
-                  Console.WriteLine("Saldo insuficiente!");
-              }
-          }              
-          
-      }
-  }
+namespace ColectivoNamespace
+{
+    public class colectivo
+    {
+        public string linea = "102 144";
+        boleto boleto = new boleto();
+        public virtual void PagarCon(tarjeta tarjeta, int precio)
+        {
+            if (tarjeta is MedioBoleto)
+            {
+                precio = ((MedioBoleto)tarjeta).precioBoleto(precio);
+            }
+            else if (tarjeta is FranquiciaCompleta)
+            {
+                precio = ((FranquiciaCompleta)tarjeta).precioBoleto(precio);
+            }
+            else
+            {
+                precio = tarjeta.precioBoleto(precio);
+            }
+
+            if (tarjeta.saldo >= precio - 480)
+            {
+                tarjeta.saldo -= precio;
+                Console.WriteLine("Pago realizado con éxito.");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+        }
+
+    }
+}
